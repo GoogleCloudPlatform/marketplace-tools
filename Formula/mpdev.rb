5b3c9e99676cea7c60351d627fd767e9e98a844d
Brian Gibbon <gibbley@google.com>
2020-07-28 21:26:55 +0000

    Update sha in brew formula

--- Formula/mpdev.rb
+++ Formula/mpdev.rb
@@ -12,12 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Homebrew formula for installing mpdev
+# Homebrew formula for installing mpdev. See ./update.sh for upgrading to newest release
 class Mpdev < Formula
   desc "Toolkit to both configure and construct artifacts needed for publishing to the Google Cloud Marketplace."
   homepage "https://github.com/GoogleCloudPlatform/marketplace-tools"
   url "https://github.com/GoogleCloudPlatform/marketplace-tools/releases/download/v0.0.0/mpdev_darwin_amd64_v0.0.0.tar.gz"
-  sha256 "2dc3bf838b10b0f386e3b0151fd3b9868a31de16d695f42b6ae97887c1c38646"
+  sha256 "6c12b516b522b24f5b8304da5cd702a437772f230465764c8f9c9073a6f83718"
 
   bottle :unneeded
 
