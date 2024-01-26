e379abe7323713a52b712309d74e48719a8a6b54
Brian Gibbon <gibbley@google.com>
2024-01-26 03:16:10 +0000

    Change wordpress example to utilize Autogen

--- examples/terraform/wordpress/marketplace_test.tfvars
+++ examples/terraform/wordpress/marketplace_test.tfvars
@@ -1,6 +1,3 @@
-goog_cm_deployment_name="test-deployment"
-tcp_443_ip_source_ranges=""
-tcp_80_ip_source_ranges=""
-enable_monitoring=true
-enable_logging=false
-wp_admin_email="user@example.com"
+goog_cm_deployment_name = "test-deployment"
+zone = "us-central1-f"
+adminEmailAddress = "default-user@example.com"
--- examples/terraform/wordpress/outputs.tf
+++ examples/terraform/wordpress/outputs.tf
@@ -1,71 +1,73 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 locals {
-  network_interface = google_compute_instance.default.network_interface[0]
+  network_interface = google_compute_instance.instance.network_interface[0]
+  instance_nat_ip   = length(local.network_interface.access_config) > 0 ? local.network_interface.access_config[0].nat_ip : null
+  instance_ip       = coalesce(local.instance_nat_ip, local.network_interface.network_ip)
 }
 
-output "instance_self_link" {
-  description = "Self-link for the Wordpress compute instance"
-  value       = google_compute_instance.default.self_link
+output "site_url" {
+  description = "Site Url"
+  value       = "https://${local.instance_ip}/"
 }
 
-output "instance_zone" {
-  description = "Zone for the wordpress compute instance"
-  value       = var.zone
+output "admin_url" {
+  description = "Admin Url"
+  value       = "https://${local.instance_ip}/wp-admin"
 }
 
-output "instance_machine_type" {
-  description = "Machine type for the wordpress compute instance"
-  value       = var.machine_type
+output "wordpress_mysql_user" {
+  description = "Username for WordPress MySQL password."
+  value       = "wordpress"
 }
 
-output "instance_nat_ip" {
-  description = "Machine type for the wordpress compute instance"
-  value       = length(local.network_interface.access_config) > 0 ? local.network_interface.access_config[0].nat_ip : null
-}
-
-output "has_external_ip" {
-  description = "Flag to indicate if the wordpress machine has an external IP"
-  value       = length(compact(local.network_interface.access_config[*].nat_ip)) > 0 ? true : false
-}
-
-output "instance_network" {
-  description = "Machine type for the wordpress compute instance"
-  value       = local.network_interface
-}
-
-output "mysql_password" {
-  description = "Password for the MySql user"
-  value       = random_password.mysql.result
+output "wordpress_mysql_password" {
+  description = "Password for WordPress MySQL."
+  value       = random_password.wordpress_mysql.result
   sensitive   = true
 }
 
-output "root_password" {
-  description = "Password for the root user"
+output "mysql_root_user" {
+  description = "Username for MySQL root password."
+  value       = "root"
+}
+
+output "mysql_root_password" {
+  description = "Password for MySQL root."
   value       = random_password.mysql_root.result
   sensitive   = true
 }
 
-output "admin_password" {
-  description = "Password for the admin user"
-  value       = random_password.mysql_admin.result
+output "wordpress_admin_user" {
+  description = "Username for WordPress Admin password."
+  value       = var.adminEmailAddress
+}
+
+output "wordpress_admin_password" {
+  description = "Password for WordPress Admin."
+  value       = random_password.wordpress_admin.result
   sensitive   = true
 }
 
-output "admin_user" {
-  description = "Admin username for Wordpress"
-  value       = var.wp_admin_email
+output "instance_self_link" {
+  description = "Self-link for the compute instance."
+  value       = google_compute_instance.instance.self_link
+}
+
+output "instance_zone" {
+  description = "Zone for the compute instance."
+  value       = var.zone
+}
+
+output "instance_machine_type" {
+  description = "Machine type for the compute instance."
+  value       = var.machine_type
+}
+
+output "instance_nat_ip" {
+  description = "External IP of the compute instance."
+  value       = local.instance_nat_ip
+}
+
+output "instance_network" {
+  description = "Self-link for the network of the compute instance."
+  value       = var.networks[0]
 }
