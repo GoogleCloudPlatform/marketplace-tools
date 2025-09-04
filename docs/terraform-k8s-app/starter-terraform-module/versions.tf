63deaabadda7758e873c342aad8d5c30b68b5f9d
skywardpixel <kyleyan@google.com>
2025-09-04 00:00:56 -0700

    Require hashicorp/google >= 5.0.0 in examples. (#107)
    5.0.0 is the minimum version that supports default_labels.

--- docs/terraform-k8s-app/starter-terraform-module/versions.tf
+++ docs/terraform-k8s-app/starter-terraform-module/versions.tf
@@ -16,6 +16,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
+      version = ">= 5.0.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
